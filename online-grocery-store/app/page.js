714cfc56fdfc974bdd16cfd5ac43b2370f0960b7
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-05-23 17:21:29 +0300

    feat: add header

--- online-grocery-store/app/page.js
+++ online-grocery-store/app/page.js
@@ -1,11 +1,6 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+// import { Button } from "@/components/ui/button";
+// import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="">
-      <h2 className="">Subscribe to our newsletter</h2>
-      <Button>Subscribe</Button>
-    </div>
-  );
+  return <div className="">Home</div>;
 }
