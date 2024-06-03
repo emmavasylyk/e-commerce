9fc2f16d51a3f50ac0372ebb069c6c6f2e520e0a
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-06-03 19:09:07 +0300

    fix; remove comment code

--- online-grocery-store/app/_components/CartItemList.jsx
+++ online-grocery-store/app/_components/CartItemList.jsx
@@ -1,20 +1,8 @@
-import { Button } from "@/components/ui/button";
 import { TrashIcon } from "lucide-react";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function CartItemList({ cartItemList, onDeleteItem }) {
-  // const [subTotal, setSubTotal] = useState(0);
-
-  // useEffect(() => {
-  //   let total = 0;
-
-  //   cartItemList.forEach((element) => {
-  //     total = total + element.amount;
-  //   });
-  //   setSubTotal(total.toFixed(2));
-  // }, [cartItemList]);
-
   return (
     <div>
       <div className="h-[500px] overflow-auto">
@@ -47,13 +35,6 @@ function CartItemList({ cartItemList, onDeleteItem }) {
           );
         })}
       </div>
-
-      {/* <div className="absolute bottom-6 w-[90%] flex flex-col">
-        <h2 className="text-lg font-bold flex justify-between items-center">
-          Subtotal <span className="">${subTotal}</span>
-        </h2>
-        <Button>Checkout</Button>
-      </div> */}
     </div>
   );
 }
