775bbc54748cbfea99d69d007599677553a5e7c5
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-06-04 16:13:00 +0300

    fix: test deploy

--- grocery-admin/config/plugins.js
+++ grocery-admin/config/plugins.js
@@ -2,10 +2,10 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
-      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
-      teamFilter: "your-team-id-on-vercel",
+      deployHook: env("VERCEL_DEPLOY_PLUGIN_HOOK"),
+      apiToken: env("VERCEL_DEPLOY_PLUGIN_API_TOKEN"),
+      appFilter: env("VERCEL_DEPLOY_PLUGIN_APP_FILTER"),
+      teamFilter: "team_1xbrTIYGP1KzJXfzdTxLZ5Rj",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
