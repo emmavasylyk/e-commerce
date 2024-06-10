ac2b31d7547bd634b4ef7594f17403d7d671d7a2
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-06-10 11:46:38 +0300

    fix url

--- online-grocery-store/app/_utils/GlobalApi.jsx
+++ online-grocery-store/app/_utils/GlobalApi.jsx
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}`,
 });
 
 const getCategory = () => axiosClient.get("/categories?populate=*");
